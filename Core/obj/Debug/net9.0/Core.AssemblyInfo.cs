de3267ddbaf2420af7a9d228c199d2888f74a742
Enmanuel Chavez <20251099@itla.edu.do>
2026-07-15 16:24:43 -0400

    Commit bien

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b8f4112803d47d6a622ad5b2ec7fa13c29b69ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+800e5f75bb213dddf9c2365c1249d968f524f0c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
